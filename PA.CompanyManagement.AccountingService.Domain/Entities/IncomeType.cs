3589a1134ee09e8c35e78088dee4976785134c0d
BarlasTR <alicancnrk@gmail.com>
2025-12-29 17:48:29 +0300

    BaseEntity Created

---
+++ PA.CompanyManagement.AccountingService.Domain/Entities/IncomeType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PA.CompanyManagement.AccountingService.Domain.Entities
+{
+    public class IncomeType
+    {
+    }
+}
---
+++ PA.CompanyManagement.Core/Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PA.CompanyManagement.Core.Domain.Entities.Base
+{
+    public abstract class BaseEntity
+    {
+        [Key] public Guid Id { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset? LastModifiedAt { get; set; }
+
+        public Guid CreatedBy { get; set; }
+        public Guid LastModifiedBy { get; set; }
+
+        public DateTimeOffset? DeletedAt { get; set; }
+        public Guid? DeletedBy { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
